0222b64f08ac51f03247acf18f412b17eb7a93ab
ladynin <ladynin@komus.net>
2025-11-05 01:26:36 +0300

    hw04-spring-shell fixing Application

--- hw04-spring-shell/src/main/java/ru/otus/hw/Application.java
+++ hw04-spring-shell/src/main/java/ru/otus/hw/Application.java
@@ -2,9 +2,7 @@ package ru.otus.hw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.shell.command.annotation.CommandScan;
 
-@CommandScan
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
