5c10169a41ec526bc63de7095b7d8c05aab724d8
ladynin <ladynin@komus.net>
2025-11-06 10:25:47 +0300

    hw04-spring-shell fixing TestServiceTest

--- hw04-spring-shell/src/test/java/ru/otus/hw/service/TestServiceTest.java
+++ hw04-spring-shell/src/test/java/ru/otus/hw/service/TestServiceTest.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
@@ -68,39 +69,39 @@ public class TestServiceTest {
     @Autowired
     private TestService testService;
     @MockitoBean
-    private QuestionDao mockCsvQuestionDao;
+    private QuestionDao mockBeanCsvQuestionDao;
     @MockitoBean
-    private LocalizedIOService mockIOService;
+    private LocalizedIOService mockBeanIOService;
     @MockitoBean
-    private TestConverter mockTestConverter;
-    @MockitoBean
+    private TestConverter mockBeanTestConverter;
+    @Mock
     private Student mockStudent;
 
     @Test
     void executeTest() {
-        given(mockCsvQuestionDao.findAll()).willReturn(EXPECTED_QUESTIONS);
+        given(mockBeanCsvQuestionDao.findAll()).willReturn(EXPECTED_QUESTIONS);
         var captor = ArgumentCaptor.forClass(Question.class);
 
-        given(mockTestConverter.questionToString(QUESTION_1)).willReturn(PRINT_QUESTION_1);
-        given(mockTestConverter.questionToString(QUESTION_2)).willReturn(PRINT_QUESTION_2);
-        given(mockTestConverter.questionToString(QUESTION_3)).willReturn(PRINT_QUESTION_3);
-        given(mockTestConverter.questionToString(QUESTION_4)).willReturn(PRINT_QUESTION_4);
-        given(mockTestConverter.questionToString(QUESTION_5)).willReturn(PRINT_QUESTION_5);
-        given(mockTestConverter.questionToString(QUESTION_6)).willReturn(PRINT_QUESTION_6);
+        given(mockBeanTestConverter.questionToString(QUESTION_1)).willReturn(PRINT_QUESTION_1);
+        given(mockBeanTestConverter.questionToString(QUESTION_2)).willReturn(PRINT_QUESTION_2);
+        given(mockBeanTestConverter.questionToString(QUESTION_3)).willReturn(PRINT_QUESTION_3);
+        given(mockBeanTestConverter.questionToString(QUESTION_4)).willReturn(PRINT_QUESTION_4);
+        given(mockBeanTestConverter.questionToString(QUESTION_5)).willReturn(PRINT_QUESTION_5);
+        given(mockBeanTestConverter.questionToString(QUESTION_6)).willReturn(PRINT_QUESTION_6);
         var questionCaptor = ArgumentCaptor.forClass(String.class);
 
-        given(mockIOService.readIntForRangeWithPromptLocalized(1, 4, PROMPT, ERROR_MESSAGE)).willReturn(1);
-        given(mockIOService.readIntForRangeWithPromptLocalized(1, 3, PROMPT, ERROR_MESSAGE)).willReturn(1);
-        given(mockIOService.readIntForRangeWithPromptLocalized(1, 2, PROMPT, ERROR_MESSAGE)).willReturn(1);
+        given(mockBeanIOService.readIntForRangeWithPromptLocalized(1, 4, PROMPT, ERROR_MESSAGE)).willReturn(1);
+        given(mockBeanIOService.readIntForRangeWithPromptLocalized(1, 3, PROMPT, ERROR_MESSAGE)).willReturn(1);
+        given(mockBeanIOService.readIntForRangeWithPromptLocalized(1, 2, PROMPT, ERROR_MESSAGE)).willReturn(1);
 
         testService.executeTestFor(mockStudent);
 
-        verify(mockIOService, times(2)).printLine("");
-        verify(mockIOService, times(1)).printLineLocalized("TestService.answer.the.questions");
-        verify(mockCsvQuestionDao, times(1)).findAll();
-        verify(mockTestConverter, times(6)).questionToString(captor.capture());
+        verify(mockBeanIOService, times(2)).printLine("");
+        verify(mockBeanIOService, times(1)).printLineLocalized("TestService.answer.the.questions");
+        verify(mockBeanCsvQuestionDao, times(1)).findAll();
+        verify(mockBeanTestConverter, times(6)).questionToString(captor.capture());
 
-        verify(mockIOService, times(8)).printLine(questionCaptor.capture());
+        verify(mockBeanIOService, times(8)).printLine(questionCaptor.capture());
         var actualPrintQuestions = questionCaptor.getAllValues();
 
         assertThat(actualPrintQuestions).isEqualTo(EXPECTED_PRINT_QUESTIONS);
